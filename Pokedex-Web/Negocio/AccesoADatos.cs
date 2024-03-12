217e00c476a899a5b5213f5bfe03eb45240f58c4
GianlucaTerzoni <gianlucaterzoni@hotmail.com>
2024-03-12 16:52:58 -0300

    configuracion de la clase acceso a datos

--- Pokedex-Web/Negocio/AccesoADatos.cs
+++ Pokedex-Web/Negocio/AccesoADatos.cs
@@ -2,10 +2,79 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.SqlClient;
 
 namespace Negocio
 {
     public class AccesoADatos
     {
+        SqlConnection conexion;
+        SqlCommand comando;
+        SqlDataReader lector;
+        SqlDataReader Lector { get
+            {
+                return lector;
+            } }
+
+        public AccesoADatos()
+        {
+            conexion = new SqlConnection("server =.\\SQLEXPRESS; database=POKEDEX_DB; integrated security=true");
+            comando = new SqlCommand();
+
+        }
+
+        public void SetearConsulta (string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void SetearStoredProcedure(string sp)
+        {
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.CommandText = sp;
+        }
+
+        public void EjecutarLectura()
+        {
+            comando.Connection = conexion;
+
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void EjecutarAccion()
+        {
+            comando.Connection = conexion;
+
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void SetearParametros(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+        public void CerrarConexion()
+        {
+            if (lector != null)
+                conexion.Close();
+            conexion.Close();
+        }
+
     }
 }
