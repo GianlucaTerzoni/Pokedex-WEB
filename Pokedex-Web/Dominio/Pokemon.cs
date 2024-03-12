bcc68935b8f8cd0e8df4718698cbf72e3b868669
GianlucaTerzoni <gianlucaterzoni@hotmail.com>
2024-03-12 16:06:47 -0300

    se agregó la clase pokemon

---
+++ Pokedex-Web/Dominio/Pokemon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Dominio
+{
+    public class Pokemon
+    {
+
+        public int Id { get; set; }
+ 
+        public int Numero { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string UrlImagen { get; set; }
+        
+
+    }
+}
